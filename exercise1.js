cca22f3e836e225877439836b8727c5319dd3d80
unknown <G00401878@atu.ie>
2023-09-22 13:59:32 +0000

    first commit

---
+++ exercise1.js
@@ -0,0 +1,23 @@
+console.log("Hello World!");
+
+function SayHello(){
+    console.log("Hello");
+}
+
+SayHello();
+
+const myFunction = function SayHi(){
+    console.log("Hi")
+}
+
+myFunction();
+
+const myHiFunction = function(){
+    console.log("Good Day!")
+}
+myHiFunction();
+
+const myComponent = ()=>{
+    console.log("Bye!")
+}
+myComponent();
